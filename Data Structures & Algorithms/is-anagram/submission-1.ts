93c24573e0f03121170867a61af43da3478319d0
Gino Valente <49806985+MrGVSV@users.noreply.github.com>
2026-05-17 17:41:53 -0700

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.ts
@@ -0,0 +1,32 @@
+class Solution {
+    private readonly ASCII_START = 'a'.charCodeAt(0);
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s: string, t: string): boolean {
+        const counts = Array.from({length: 26}, () => 0);
+        for (const char of s) {
+            const index = char.toLowerCase().charCodeAt(0) - this.ASCII_START;
+            counts[index] += 1;
+        }
+
+        for (const char of t) {
+            const index = char.toLowerCase().charCodeAt(0) - this.ASCII_START;
+            counts[index] -= 1;
+
+            if (counts[index] < 0) {
+                return false;
+            }
+        }
+
+        for (const count of counts) {
+            if (count > 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
