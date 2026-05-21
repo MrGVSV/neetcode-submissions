9032660a684c0ec7bfb089f1c8f1d489e8422eff
Gino Valente <49806985+MrGVSV@users.noreply.github.com>
2026-05-21 00:11:28 -0700

    Add: is-palindrome - submission-5

---
+++ Data Structures & Algorithms/is-palindrome/submission-5.ts
@@ -0,0 +1,58 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isPalindrome(s: string): boolean {
+        if (s.length === 0) {
+            return false;
+        }
+        if (s.length === 1) {
+            return true;
+        }
+
+        let leftIndex = 0;
+        let rightIndex = s.length - 1;
+
+        while (leftIndex < rightIndex) {
+            while (!this.isAlphanumeric(s[leftIndex]) && leftIndex < rightIndex) {
+                leftIndex++;
+            }
+
+            while (!this.isAlphanumeric(s[rightIndex]) && leftIndex < rightIndex) {
+                rightIndex--;
+            }
+
+            const left = this.toNormalized(s[leftIndex]);
+            const right = this.toNormalized(s[rightIndex]);
+            if (left !== right) {
+                return false;
+            }
+
+            leftIndex++;
+            rightIndex--;
+        }
+
+        return true;
+    }
+
+    isAlphanumeric(s: string): boolean {
+        return (s >= "a" && s <= "z") || (s >= "A" && s <= "Z") || (s >= "0" && s <= "9");
+    }
+
+    toNormalized(s: string): string {
+        if (s >= "a" && s <= "z") {
+            return s;
+        }
+
+        if (s >= "0" && s <= "9") {
+            return s;
+        }
+
+        if (s >= "A" && s <= "Z") {
+            return String.fromCharCode(s.charCodeAt(0) + 32);
+        }
+
+        return "";
+    }
+}
