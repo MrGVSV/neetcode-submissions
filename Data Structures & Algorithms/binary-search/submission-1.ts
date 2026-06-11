505f6dfc11244de86807004a9091fda7cb9c2665
Gino Valente <49806985+MrGVSV@users.noreply.github.com>
2026-06-11 12:14:08 -0700

    Add: binary-search - submission-1

---
+++ Data Structures & Algorithms/binary-search/submission-1.ts
@@ -0,0 +1,30 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number}
+     */
+    search(nums: number[], target: number): number {
+        return this.searchInternal(nums, target, 0, nums.length);
+    }
+
+    private searchInternal(nums: number[], target: number, start: number, end: number) {
+        switch (end - start) {
+            case 0:
+                return -1;
+            case 1:
+                return nums[start] === target ? start : -1;
+            default:
+                const index = start + Math.floor((end - start) / 2);
+                const mid = nums[index];
+                
+                if (mid === target) {
+                    return index;
+                }
+
+                return mid > target
+                    ? this.searchInternal(nums, target, start, index)
+                    : this.searchInternal(nums, target, index, end);
+        }
+    }
+}
